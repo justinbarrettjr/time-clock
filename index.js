1359c17a56136cabb0b28b1d31161d6eab078583
Justin Barrett <justinbarrettjr@gmail.com>
2022-01-20 18:19:08 -0500

    v1.0

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+let current_month = null
+let dark_mode = false
+
 class TimeClock {
     constructor() {
         this.clocked_in = false
@@ -10,7 +14,10 @@ class TimeClock {
         if(this.clocked_in) return 'Already clocked in'
 
         this.clocked_in = true
-        this.time_in = Date.now()
+
+        if(!ignore_storage) this.time_in = Date.now()
+        else this.time_in = storage.today[storage.today.length-1].in
+
         if(this.button != null){
             this.button.innerText = 'Clock Out'
             this.button.style.background = '#dd2121'
@@ -20,8 +27,6 @@ class TimeClock {
         if(!ignore_storage){
             storage.today.push( {in: this.time_in, out: null} )
             storage.update()
-        }else{
-            this.time_in = storage.today[storage.today.length-1].in
         }
 
         return `Clocked in at: ${parse_time(this.time_in)}`
@@ -44,17 +49,50 @@ class TimeClock {
     }
 }
 
+class Day{
+    constructor(key, value) {
+        this.key = key
+        this.month = key.split('_')[0]
+        this.day = key.split('_')[1]
+        this.year = key.split('_')[2]
+        this.data = value
+        this.div = this.print()
+    }
+    
+    print() {
+        if(current_month != this.month){
+            let yr = ''
+            if(this.year != new Date().getFullYear())
+                yr = ' '+this.year
+            current_month = this.month
+            new_month(MONTHS[this.month]+yr)
+        }
+        
+        let div = document.createElement('fieldset')
+        let sub_div = document.createElement('legend')
+        sub_div.innerHTML = this.day+'<span></span>'
+        div.append(sub_div)
+        sub_div = document.createElement('div')
+        sub_div.classList.add('space')
+        div.append(sub_div)
+        document.querySelector('#container').append(div)
+        return div
+    }
+}
 
 let clock = new TimeClock()
 let storage = new LocalStorage()
-let div
+let today_div
+let total_time_today = 0
+let current_time_clock = null
 
 document.addEventListener("DOMContentLoaded", function(){
     init()
 });
 
 function init() {
-    console.log('initialized')
+    resize()
+    console.debug('initialized')
     clock.button = document.querySelector('#time_clock_button')
     clock.button.onclick = function() {
         if(clock.clocked_in)
@@ -63,7 +101,6 @@ function init() {
             console.log(clock.clock_in())
     }
 
-            
     // Set object for today if not already present
     if(storage.obj[today()] == null){
         storage.obj[today()] = []
@@ -71,46 +108,37 @@ function init() {
     }
     storage.today = storage.obj[today()]
 
-    console.log('Today: '+today())
+    console.debug('Today: '+today())
+    console.debug(storage.obj)
 
-    // Show Times Clocked in
-    let total_time_today = 0
-    storage.today.forEach(time => {
-        let time_in = new Date(time.in)
-        time_in = time_in.getHours() * 60 + time_in.getMinutes()
 
-        let time_out
-        if(time.out != null)
-            time_out = new Date(time.out)
-        else
-            time_out = new Date()
+    for (const [key, value] of Object.entries(storage.obj)) {
+        let day = new Day(key, value)
+        if(day.key == today())
+            today_div = day.div
+
+        // Show Times Clocked in
+        day.data.forEach(time => {
+            let time_in = get_total_minutes(new Date(time.in))
         
-        time_out = time_out.getHours() * 60 + time_out.getMinutes()
+            let time_out
+            if(time.out != null) time_out = get_total_minutes(new Date(time.out))
+            else time_out = get_total_minutes(new Date())
 
-        div = document.createElement('div')
-        div.classList.add('schedule')
-        div.style.left = Math.floor((time_in / 1440) * 100) + '%'
-        div.style.width = Math.floor(((time_out - time_in) / 1440) * 100) + '%'
+            new_div(time, false, day.div)
 
-        let string_time_in = parse_time(time.in)
-        let string_time_out = parse_time(time.out)
-        if(time.out == null)
-            string_time_out = 'now'
+            total_time_today += time_out - time_in
+        })
+        
+        // Show Hours in today
+        total_hours(total_time_today, day.div)
 
-        div.innerText = `${string_time_in} - ${string_time_out}`
-        div.title = div.innerText
-        document.querySelector('#today').children[1].append(div)
+    }
 
-        total_time_today += time_out - time_in
-    })
-    
-    let suffix = 'hr'
-    if(total_time_today < 60)
-        suffix = 'min'
-    else
-        total_time_today = Math.floor(total_time_today / 60 * 10) / 10
-
-    document.querySelector('#today').children[0].children[0].innerText = total_time_today + ' ' + suffix
+    if(today_div == null){
+        let day = new Day(today(), [])
+        today_div = day.div
+    }
 
     // Auto Clock In
     let auto_clock_in = false
@@ -118,35 +146,107 @@ function init() {
     else if(storage.today[storage.today.length-1].out == null) auto_clock_in = true
     if(auto_clock_in) clock.clock_in(true)
 
-    // Testing
+    // Dark Mode
+    if(storage.get('dark_mode'))
+        toggle_dark_mode()
 
-    var start = new Date();
-    start.setDate(19)
-    start.setHours(0,0,0,0);
-
-    var end = new Date();
-    end.setDate(19)
-    end.setHours(23,59,59,999);
-
-    // alert( start.toUTCString() + ':' + end.toUTCString() );
 
 }
 
+function resize() {
+    document.querySelector('#container').style.height = window.innerHeight - 230 + 'px'
+}
+
+function toggle_dark_mode() {
+    if(dark_mode != false){
+        dark_mode.remove()
+        dark_mode = false
+        storage.set('dark_mode', false)
+    }else{
+        let div = document.createElement('link')
+        div.rel = 'stylesheet'
+        div.href = 'dark_mode.css'
+        document.head.append(div)
+        dark_mode = div
+        storage.set('dark_mode', true)
+    }
+}
+
+function new_month(month) {
+    let div = document.createElement('h1')
+    div.innerHTML = `<span>${month}</span>`
+    document.querySelector('#container').append(div)
+}
+
+function total_hours(total_time, day = null) {
+    let suffix = 'hr'
+    if(total_time < 60)
+        suffix = 'min'
+    else
+    total_time = Math.floor(total_time / 60 * 10) / 10
+
+    if(day != null)
+        day.children[0].children[0].innerText = total_time + ' ' + suffix
+    else
+        console.error('day = null')
+}
+
+function get_total_minutes(time){
+    return time.getHours() * 60 + time.getMinutes()
+}
+
+function new_div(time, existing_div = false, append = null, clas = 'time_in') {
+
+    let time_in = get_total_minutes(new Date(time.in))
+    
+    let time_out
+    if(time.out != null) time_out = get_total_minutes(new Date(time.out))
+    else time_out = get_total_minutes(new Date())
+
+    // Create Div
+    if(!existing_div){
+        div = document.createElement('div')
+        div.classList.add(clas)
+    }else
+        div = existing_div
+
+    div.style.left = Math.floor((time_in / 1440) * 100) + '%'
+    div.style.width = Math.floor(((time_out - time_in) / 1440) * 100) + '%'
+
+    // Write String for time range
+    let string_time_in = parse_time(time.in)
+    let string_time_out = parse_time(time.out)
+    if(time.out == null)
+        string_time_out = 'now'
+
+    div.innerText = `${string_time_in} - ${string_time_out}`
+    div.title = div.innerText
+
+    // Append Div
+    if(append != null)
+        append.children[1].append(div)
+
+    return div
+}
 
 function clocked_in() {
     clock.button.innerText = parse_time_frame(Date.now() - clock.time_in)
 
-
     let time_in = new Date(clock.time_in)
-    time_in = time_in.getHours() * 60 + time_in.getMinutes()
-
     let time_out = new Date()
-    time_out = time_out.getHours() * 60 + time_out.getMinutes()
 
-    div.style.width = Math.floor(((time_out - time_in) / 1440) * 100) + '%'
+    if(current_time_clock == null)
+    current_time_clock = new_div({in: time_in, out: time_out}, false, today_div)
+    else
+        new_div({in: time_in, out: time_out}, current_time_clock, today_div)
+
+    total_hours(total_time_today + (get_total_minutes(time_out) - get_total_minutes(time_in)), today_div)
 
     if(clock.clocked_in) setTimeout(clocked_in, 1000)
-    else clock.button.innerText = 'Clock In'
+    else{ 
+        clock.button.innerText = 'Clock In'
+        div = null
+    }
 }
 
 function parse_time(time) {
@@ -164,9 +264,10 @@ function parse_time_frame(time) {
     let mm = ss / 60
     let hh = mm / 60
 
-    ss = Math.floor(ss)
+    ss = Math.floor(ss)%60
     if(ss < 10) ss = `0${ss}`
-    mm = Math.floor(mm)
+    
+    mm = Math.floor(mm)%60
     if(mm < 10) mm = `0${mm}`
     hh = Math.floor(hh)
     if(hh < 10) hh = `0${hh}`
